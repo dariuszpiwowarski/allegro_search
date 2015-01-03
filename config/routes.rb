1fc997ccb91ac4bc085e9dc770a061dcf491ee00
Dariusz Piwowarski <piwowarskidariusz@gmail.com>
2015-01-03 18:06:08 +0100

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'search' => 'search#index'
   get 'search/search' => 'search#search'
 
-  resources :auctions only:, [:index, :create, :destroy]
+  resources :auctions, only: [:index, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
